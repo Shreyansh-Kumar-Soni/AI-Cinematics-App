d594e38d98a5a371634e4df99420cb946bf0e0fd
Shreyansh-77 <shreyanshsoni9136@gmail.com>
2025-07-30 09:32:42 +0530

    adding assets

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Slot } from 'expo-router'
+import { Slot, Stack } from 'expo-router'
 
-const _layout = () => {
-  return <Slot/>
+const RootLayout = () => {
+  return (
+    <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  )
 }
 
-export default _layout
+export default RootLayout
 
